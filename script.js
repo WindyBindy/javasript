cd7e2fc25be0a193069630a848f2cf4993a63aab
WindyBindy <nikita2021novsk@gmail.com>
2025-03-26 17:38:55 +0100

    finish

---
+++ script.js
@@ -0,0 +1,19 @@
+const age = 12
+console.log(age)
+const name = 'Nikita'
+console.log(name)
+const isStudent = true
+const myString = 'Стремитесь не к успеху, а к ценностям, которые он дает'
+const myNumber = 234234
+console.log(myNumber + 10)
+const myNull = null
+console.log(typeof myNull)
+
+const userName = prompt('Введіть своє ім’я')
+alert(userName)
+
+console.log(typeof userName)
+confirm('Ви дійсно хочете покинути сторінку?')
+console.log(userName)
+alert('Увага! Дія може бути небезпечною.')
+confirm('Ви впевнені, що хочете продовжити')
